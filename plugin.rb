44bcd6004d4ef6bd2147927edd40e4c11c1e712f
zhangml123 <s15532292192@163.com>
2020-03-06 20:54:38 +0800

    update files

--- plugin.rb
+++ plugin.rb
@@ -5,7 +5,7 @@
 # url: https://github.com/zhangml123/discourse_essence_menu
 
 
-register_asset "javascripts/routes/essence-map.js.es6"
+register_asset "javascripts/routes/essence-route-map.js.es6"
 register_asset "javascripts/routes/essence.js.es6"
 enabled_site_setting :discourse_essence_menu
 after_initialize do
