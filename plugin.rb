db3f14b9bfdf7fad149bafb7554e5d3f16621f18
zhangml123 <s15532292192@163.com>
2020-03-07 11:29:50 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -16,13 +16,13 @@ class ::TopicQuery
   SORTABLE_MAPPING["essence"] = "custom_fields.event_start"
 
   def list_essence
-    @options[:order] = "essence"
+    #@options[:order] = "essence"
     #topics = create_list(:essence, ascending: "true")
     #topics = create_list(:essence, unordered: false) do |topics|
      # topics.where('pinned_globally IS NOT NULL OR pinned_until > now()').order('pinned_at DESC')
     #end
-    topics = create_list(:essence) { |l| l.where('pinned_globally IS NOT NULL OR pinned_until > now()').order('pinned_at DESC') }
-    
+    #topics = create_list(:essence) { |l| l.where('pinned_globally IS NOT NULL OR pinned_until > now()').order('pinned_at DESC') }
+    topics = create_list(:essence) { |l| l.where('tu.bookmarked') }
   end
 end   
 
