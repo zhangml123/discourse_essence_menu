d5c8cc402b4387426a315a815e281d60acafd5eb
zhangml123 <s15532292192@163.com>
2020-03-07 11:22:24 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -20,8 +20,9 @@ class ::TopicQuery
     #topics = create_list(:essence, ascending: "true")
     #topics = create_list(:essence, unordered: false) do |topics|
      # topics.where('pinned_globally IS NOT NULL OR pinned_until > now()').order('pinned_at DESC')
+    #end
     topics = create_list(:essence) { |l| l.where('pinned_globally IS NOT NULL OR pinned_until > now()').order('pinned_at DESC') }
-    end
+    
   end
 end   
 
